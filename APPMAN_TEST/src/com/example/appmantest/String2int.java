5611f787c88fa31f53a3ff84c09abe629807fa1a
pek_pariwat02@hotmail.com <pek_pariwat02@hotmail.com>
2021-08-16 21:39:51 +0700

    update type name

--- APPMAN_TEST/src/com/example/appmantest/String2int.java
+++ APPMAN_TEST/src/com/example/appmantest/String2int.java
@@ -8,11 +8,11 @@ public class String2int {
         int sum = 0;
         String Text="";
         Scanner String2int = new Scanner(System.in);  // Create a Scanner String2int
-        System.out.print("Input: ");
-        String userName = String2int.nextLine();
-        for (int i = 0; i < userName.length();i++) {
-            if(userName.charAt(i) >= 48 && userName.charAt(i) <= 57){
-                Text = Text + userName.charAt(i);
+        System.out.print("Input: "); // Input Text Ex: abc573
+        String String2int2 = String2int.nextLine();  //Keep input Value in String2int2
+        for (int i = 0; i < String2int2.length();i++) {  //Loop Check Value in String2int2
+            if(String2int2.charAt(i) >= 48 && String2int2.charAt(i) <= 57){  //If String2int2 is integer then add String2int2 to Text
+                Text = Text + String2int2.charAt(i);
             }
         }
         // Read user input
